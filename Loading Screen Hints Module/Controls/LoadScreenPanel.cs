9b97fcc0891c3349fd7ab79e7c1b6d66183bf586
Andreas G <13819164+agaertner@users.noreply.github.com>
2022-05-10 13:19:54 +0200

    add loading screen hints

---
+++ Loading Screen Hints Module/Controls/LoadScreenPanel.cs
@@ -0,0 +1,93 @@
+﻿using Blish_HUD.Controls;
+using System;
+using Blish_HUD;
+using Blish_HUD.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Loading_Screen_Hints_Module.Controls
+{
+    public class LoadScreenPanel : Container {
+
+        public const int TOP_PADDING = 20;
+        public const int RIGHT_PADDING = 40;
+
+        private static readonly Texture2D _textureBackgroundLoadScreenPanel;
+
+        static LoadScreenPanel() {
+            _textureBackgroundLoadScreenPanel = LoadingScreenHintsModule.Instance.ContentsManager.GetTexture("background_loadscreenpanel.png");
+        }
+
+        public Glide.Tween Fade { get; private set; }
+
+        public Control LoadScreenTip;
+
+        public LoadScreenPanel() {
+            UpdateLocation(null, null);
+
+            Graphics.SpriteScreen.Resized += UpdateLocation;
+            Disposed += OnDisposed;
+        }
+
+        public void FadeOut() {
+
+            if (Opacity != 1.0f) return;
+
+            float duration = 2.0f;
+
+            if (LoadScreenTip != null) {
+                if (LoadScreenTip is GuessCharacter tip) {
+                    tip.Result = true;
+                    duration = duration + 3.0f;
+
+                } else if (LoadScreenTip is Narration narration) {
+                    duration = duration + narration.ReadingTime;
+
+                } else if (LoadScreenTip is GamingTip selected) {
+                    duration = duration + selected.ReadingTime;
+                }
+            }
+
+            Fade = Animation.Tweener.Tween(this, new { Opacity = 0.0f }, duration);
+            Fade.OnComplete(this.Dispose);
+        }
+        protected override void OnLeftMouseButtonReleased(MouseEventArgs e) {
+            if (Opacity != 1.0) return;
+            GameService.Animation.Tweener.Tween(this, new { Opacity = 0.0f }, 0.2f);
+        }
+
+        protected override void OnRightMouseButtonReleased(MouseEventArgs e)
+        {
+            if (Opacity != 0.0f) return;
+            GameService.Animation.Tweener.Tween(this, new { Opacity = 1.0f }, 0.2f);
+            base.OnRightMouseButtonReleased(e);
+        }
+
+        protected override CaptureType CapturesInput() {
+            return CaptureType.Mouse;
+        }
+
+        private void OnDisposed(object sender, EventArgs e)
+        {
+            if (LoadScreenTip != null)
+            {
+                if (LoadScreenTip is GuessCharacter)
+                {
+                    GuessCharacter selected = (GuessCharacter)LoadScreenTip;
+                    selected.CharacterImage.Dispose();
+                }
+                LoadScreenTip.Dispose();
+            }
+        }
+
+        private void UpdateLocation(object sender, EventArgs e) {
+            this.Location = new Point((Graphics.SpriteScreen.Width / 2 - this.Width / 2), (Graphics.SpriteScreen.Height  / 2 - this.Height / 2) + 300);
+        }
+
+        public override void PaintBeforeChildren(SpriteBatch spriteBatch, Rectangle bounds) {
+            spriteBatch.DrawOnCtrl(this, _textureBackgroundLoadScreenPanel, bounds);
+        }
+
+    }
+
+}
---
+++ Loading Screen Hints Module/LoadingScreenHintsModule.cs
@@ -0,0 +1,175 @@
+﻿using Blish_HUD;
+using Blish_HUD.Modules;
+using Blish_HUD.Modules.Managers;
+using Blish_HUD.Settings;
+using Loading_Screen_Hints_Module.Controls;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loading_Screen_Hints_Module
+{
+
+    [Export(typeof(Module))]
+    public class LoadingScreenHintsModule : Module {
+
+        internal static readonly Logger Logger = Logger.GetLogger(typeof(LoadingScreenHintsModule));
+
+        internal static LoadingScreenHintsModule Instance;
+
+        // Service Managers
+        internal SettingsManager    SettingsManager    => this.ModuleParameters.SettingsManager;
+        internal ContentsManager    ContentsManager    => this.ModuleParameters.ContentsManager;
+        internal DirectoriesManager DirectoriesManager => this.ModuleParameters.DirectoriesManager;
+        internal Gw2ApiManager      Gw2ApiManager      => this.ModuleParameters.Gw2ApiManager;
+
+        // Settings
+        private SettingEntry<HashSet<int>[]> SeenHints;
+
+        // Controls
+        private LoadScreenPanel LoadScreenPanel;
+        private bool Created;
+
+        // Shuffle
+        private HashSet<int> ShuffledHints;
+        private HashSet<int> SeenGamingTips;
+        private HashSet<int> SeenNarrations;
+        private HashSet<int> SeenGuessCharacters;
+
+        private Random Randomize;
+
+        [ImportingConstructor]
+        public LoadingScreenHintsModule([Import("ModuleParameters")] ModuleParameters moduleParameters) : base(moduleParameters) {
+            Instance = this;
+        }
+
+        protected override void DefineSettings(SettingCollection settings)
+        {
+            var selfManagedSettings = settings.AddSubCollection("selfManaged", false, false);
+            SeenHints = selfManagedSettings.DefineSetting("seenHints", new HashSet<int>[3]);
+        }
+
+        protected override void Initialize() {
+            this.Randomize = new Random();
+            this.ShuffledHints = new HashSet<int>();
+            this.SeenGamingTips = SeenHints.Value[0] ?? new HashSet<int>();
+            this.SeenNarrations = SeenHints.Value[1] ?? new HashSet<int>();
+            this.SeenGuessCharacters = SeenHints.Value[2] ?? new HashSet<int>();
+        }
+
+        protected override async Task LoadAsync()
+        {
+             /* NOOP */
+        }
+
+        protected override void OnModuleLoaded(EventArgs e) {
+            base.OnModuleLoaded(e);
+        }
+
+        protected override void Update(GameTime gameTime) {
+
+            if (!GameService.GameIntegration.Gw2Instance.IsInGame)
+            {
+                if (!this.Created)
+                {
+                    NextHint(); 
+                    this.Created = true;
+                }
+            }
+            else
+            {
+                if (LoadScreenPanel != null && LoadScreenPanel.Fade == null) {
+                    if (LoadScreenPanel.Opacity == 0.0f) {
+                        LoadScreenPanel.Dispose();
+                        LoadScreenPanel = null;
+                    } else {
+                        LoadScreenPanel.FadeOut();
+                    }
+                }
+                Created = false;
+            }
+        }
+
+        protected override void Unload() {
+            Instance = null;
+            LoadScreenPanel?.Dispose();
+        }
+
+        private void Save() {
+            SeenHints.Value = new []{ SeenGamingTips, SeenNarrations, SeenGuessCharacters };
+        }
+
+        public void NextHint()
+        {
+            LoadScreenPanel?.Dispose();
+
+            int total = 3;
+            int count = ShuffledHints.Count;
+            if (count >= total) { ShuffledHints.Clear(); count = 0; }
+            var range = Enumerable.Range(1, total).Where(i => !ShuffledHints.Contains(i));
+            int index = Randomize.Next(0, total - count - 1);
+            int hint = range.ElementAt(index);
+
+            ShuffledHints.Add(hint);
+
+            LoadScreenPanel = new LoadScreenPanel
+            {
+                Parent = GameService.Graphics.SpriteScreen,
+                Size = new Point(600, 200),
+                Location = new Point((GameService.Graphics.SpriteScreen.Width / 2 - 300), (GameService.Graphics.SpriteScreen.Height / 2 - 100) + 300)
+            };
+
+            switch (hint)
+            {
+                case 1:
+
+                    total = GamingTip.Tips.Count;
+                    count = SeenGamingTips.Count;
+                    if (count >= total) { SeenGamingTips.Clear(); count = 0; }
+                    range = Enumerable.Range(0, total).Where(i => !SeenGamingTips.Contains(i));
+                    index = Randomize.Next(0, total - count);
+                    hint = range.ElementAt(index);
+
+                    SeenGamingTips.Add(hint);
+                    LoadScreenPanel.LoadScreenTip = new GamingTip(hint) { Parent = LoadScreenPanel, Size = LoadScreenPanel.Size, Location = new Point(0, 0) };
+
+                    break;
+
+                case 2:
+
+                    total = Narration.Narratives.Count;
+                    count = SeenNarrations.Count;
+                    if (count >= total) { SeenNarrations.Clear(); count = 0; }
+                    range = Enumerable.Range(0, total).Where(i => !SeenNarrations.Contains(i));
+                    index = Randomize.Next(0, total - count);
+                    hint = range.ElementAt(index);
+
+                    SeenNarrations.Add(hint);
+                    LoadScreenPanel.LoadScreenTip = new Narration(hint) { Parent = LoadScreenPanel, Size = LoadScreenPanel.Size, Location = new Point(0, 0) };
+
+                    break;
+
+                case 3:
+
+                    total = GuessCharacter.Characters.Count;
+                    count = SeenGuessCharacters.Count;
+                    if (count >= total) { SeenGuessCharacters.Clear(); count = 0; }
+                    range = Enumerable.Range(0, total).Where(i => !SeenGuessCharacters.Contains(i));
+                    index = Randomize.Next(0, total - count);
+                    hint = range.ElementAt(index);
+
+                    SeenGuessCharacters.Add(hint);
+                    LoadScreenPanel.LoadScreenTip = new GuessCharacter(hint, LoadScreenPanel) { Location = new Point(0, 0) };
+
+                    break;
+
+                default:
+                    throw new NotSupportedException();
+            }
+            this.Save();
+        }
+    }
+}
